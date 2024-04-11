76792e0705a4c0fb053c2eee7ada2262dd886230
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-04-11 15:48:15 +0200

    feat: adding Vine structure and start header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,66 @@
+<script>
+export default{
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+            <li>
+                <a href="#">home</a>
+            </li>
+        </ul>
+    </nav>
+
+</template>
+
+<style scoped lang="scss">
+nav{
+    display: flex;
+    justify-content: end;
+
+    ul{
+    display: flex;
+    }
+
+    a{
+        color: black;
+        padding: 10px;
+        font-size: 13px;
+    }
+}
+
+
+</style>
